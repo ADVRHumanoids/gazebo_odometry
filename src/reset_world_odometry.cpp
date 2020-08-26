429df50d7b19296bbecfc38e56c1b2e82d30d715
Davide Antonucci <davide.antonucci@iit.it>
2020-08-26 15:22:00 +0200

    Added the code to check the error status of the frames.

--- src/reset_world_odometry.cpp
+++ src/reset_world_odometry.cpp
@@ -11,16 +11,6 @@ static tf::StampedTransform w1_T_r1, w1_T_r2,trasf_reset;
 
 bool service_reset_world(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res) {
 
- cout << " xp1: " << w1_T_r1.getOrigin().getX() << " yp1: " << w1_T_r1.getOrigin().getY() << " zp1: " << w1_T_r1.getOrigin().getZ() << endl;
- cout << " xo1: " << w1_T_r1.getRotation().getX() << " yo1: " << w1_T_r1.getRotation().getY() << " zo1: " << w1_T_r1.getRotation().getZ() << " w1: " << w1_T_r1.getRotation().getW() << endl;
- cout << " xp2: " << w1_T_r2.getOrigin().getX() << " yp2: " << w1_T_r2.getOrigin().getY() << " zp2: " << w1_T_r2.getOrigin().getZ() << endl;
- cout << " xo2: " << w1_T_r2.getRotation().getX() << " yo2: " << w1_T_r2.getRotation().getY() << " zo2: " << w1_T_r2.getRotation().getZ() << " w2: " << w1_T_r2.getRotation().getW() << endl;
-
- trasf_reset.setData(w1_T_r1.inverse()*w1_T_r2);
- 
- cout << " xpe: " << trasf_reset.getOrigin().getX() << " ype: " << trasf_reset.getOrigin().getY() << " zpe: " << trasf_reset.getOrigin().getZ() << endl;
- cout << " xeo: " << trasf_reset.getRotation().getX() << " yeo: " << trasf_reset.getRotation().getY() << " zoe: " << trasf_reset.getRotation().getZ() << " we: " << trasf_reset.getRotation().getW() << endl;
- 
  reset_request=true;
  return true;
 }
@@ -61,8 +51,24 @@ int main(int argc, char **argv)
         }
         if(reset_request)
         {
-            reset_request=false;
+        
+            cout << " xp1: " << w1_T_r1.getOrigin().getX() << " yp1: " << w1_T_r1.getOrigin().getY() << " zp1: " << w1_T_r1.getOrigin().getZ() << endl;
+            cout << " xo1: " << w1_T_r1.getRotation().getX() << " yo1: " << w1_T_r1.getRotation().getY() << " zo1: " << w1_T_r1.getRotation().getZ() << " w1: " << w1_T_r1.getRotation().getW() << endl;
+            cout << " xp2: " << w1_T_r2.getOrigin().getX() << " yp2: " << w1_T_r2.getOrigin().getY() << " zp2: " << w1_T_r2.getOrigin().getZ() << endl;
+            cout << " xo2: " << w1_T_r2.getRotation().getX() << " yo2: " << w1_T_r2.getRotation().getY() << " zo2: " << w1_T_r2.getRotation().getZ() << " w2: " << w1_T_r2.getRotation().getW() << endl;
+
+            trasf_reset.setData(w1_T_r1.inverse()*w1_T_r2);
+ 
+            cout << " xpe: " << trasf_reset.getOrigin().getX() << " ype: " << trasf_reset.getOrigin().getY() << " zpe: " << trasf_reset.getOrigin().getZ() << endl;
+            cout << " xeo: " << trasf_reset.getRotation().getX() << " yeo: " << trasf_reset.getRotation().getY() << " zoe: " << trasf_reset.getRotation().getZ() << " we: " << trasf_reset.getRotation().getW() << endl;        
             
+            if((abs(trasf_reset.getOrigin().getX()) <= 0.001)&&(abs(trasf_reset.getOrigin().getY()) <= 0.001)&&(abs(trasf_reset.getOrigin().getZ()) <= 0.001))
+                if((abs(trasf_reset.getRotation().getX()) <= 0.001)&&(abs(trasf_reset.getRotation().getY()) <= 0.001)&&(abs(trasf_reset.getRotation().getZ()) <= 0.001))
+                    if((trasf_reset.getRotation().getW() >= 0.95)&&(trasf_reset.getRotation().getW() <=1))
+                        reset_request=false;
+            
+            if(reset_request)
+            {
             rst_world.request.new_world.position.x=trasf_reset.getOrigin().getX();
             rst_world.request.new_world.position.y=trasf_reset.getOrigin().getY();
             rst_world.request.new_world.position.z=trasf_reset.getOrigin().getZ();
@@ -71,6 +77,7 @@ int main(int argc, char **argv)
             rst_world.request.new_world.orientation.y =trasf_reset.getRotation().getY();
             rst_world.request.new_world.orientation.z =trasf_reset.getRotation().getZ();
             client.call(rst_world);
+            }
             
         }
 
